3d01a60127117b9bd06e4526a4e53f1e14da6b62
Mehdi ABDERRAZAK <mehdi.abderrazak@etu.univ-lyon1.fr>
2025-11-06 12:12:44 +0100

    Ajout du modèle businessCharacter et mise en place des animations, des mouvements de base et de la rotation de la camera (à terminer)

---
+++ scripts/CameraController.gd
@@ -0,0 +1,31 @@
+extends Node3D
+
+@export var bodyNode : Node3D
+@export var cameraPanSpeed : float
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var delta = event.relative
+		
+		var newY = bodyNode.rotation_degrees.y + (-delta.x * cameraPanSpeed)
+		var newX = bodyNode.rotation_degrees.x + (delta.y * cameraPanSpeed)
+		
+		newX = clampf(newX, -60, 60)
+		
+		bodyNode.rotation_degrees = Vector3(0, newY, 0)
+		
+		rotation_degrees = Vector3(newX, rotation_degrees.y, 0)
+		
+		if event.is_action_pressed("ui_cancel"):
+			if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+			else :
+				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+		
+		
+		
+		
